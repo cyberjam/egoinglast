c9739792125b0916ab56ded87d6875637dbad5bc
cyberjam <jamsince1994@naver.com>
2022-06-29 11:02:38 +0900

    header 만들기 - Header  Tag 파일 분리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Link } from "react-router-dom";
-
-function Header() {
-  return (
-    <header>
-      <h1>
-        <Link to="/">WEB</Link>
-      </h1>
-    </header>
-  );
-}
+import { Header } from "./Header";
 
 function App() {
   return (
