58d63706c038e0f9fc587bb05c45185c0410fe6d
cyberjam <jamsince1994@naver.com>
2022-06-29 10:49:32 +0900

    header 만들기 - Link Tag

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <Link to="/">WEB</Link>
+      </h1>
+    </header>
+  );
+}
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <Header></Header>Hi
+    </div>
+  );
 }
 
 export default App;
