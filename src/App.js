a43673097edc6ab778fe16d64d095b7a3af5460f
cyberjam <jamsince1994@naver.com>
2022-06-29 11:04:20 +0900

    nav 만들기 - init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./Header";
+import { Link } from "react-router-dom";
 
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <Link to="/read/1">html</Link>
+        </li>
+      </ol>
+    </nav>
+  );
+}
 function App() {
   return (
     <div>
